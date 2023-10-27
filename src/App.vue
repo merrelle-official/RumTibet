fa09496ceffab324de4e8e5da3dc689a596eadd5
merrelle <vovachern18@list.ru>
2023-10-28 02:27:53 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="body">
+  <img src="./assets/img/hear_background_img.png" alt="Задний фон горы" class="hear_background_img">
+  <div class="main">
+    <Header class="header" :names_of_pages="names_of_pages"></Header>
+  </div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+  },
+  data() {
+    return {
+      names_of_pages: ['Главная', 'Про гида', 'Программа тура', 'Стоимость', 'Блог', 'Контакты'],
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './styles/App.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div class="header_main">
+    <a href="#"><img src="../assets/img/logo.svg" alt="Логотип" class="logo_img"></a>
+    <nav class="header_nav">
+        <ul class="header_list">
+            <li class="list_component" v-for="(name, index) in names_of_pages" :key="index"><a href="#">{{ names_of_pages[index] }}</a></li>
+        </ul>
+    </nav>
+    <button class="consult_btn">Консультация</button>
+    
+</div>
+
+</template>
+
+<script>
+    export default{
+        name: 'MHeader',
+        props: {
+            names_of_pages: {
+                type: Array
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/Header.scss";
+</style>
